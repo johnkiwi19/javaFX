38b530650d33ee61174712d5486455847a0ad51b
john <john@localhost>
2018-06-24 16:44:03 +1200

    Added a Text Field to the grid

--- src/hi/Hello.java
+++ src/hi/Hello.java
@@ -10,6 +10,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
@@ -38,6 +39,9 @@ public class Hello extends Application {
 
         Label userName = new Label("User Name:");
         grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
 
         Scene scene = new Scene(grid, 300, 275);
         primaryStage.setScene(scene);
