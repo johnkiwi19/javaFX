794fe229bb94c94e944600d42e114a5b306fb7b3
john <john@localhost>
2018-06-24 16:12:13 +1200

    Commented out the Button Ran the class

--- src/hi/Hello.java
+++ src/hi/Hello.java
@@ -21,18 +21,18 @@ public class Hello extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//            
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+//        root.getChildren().add(btn);
         
         Scene scene = new Scene(root, 300, 250);
         
