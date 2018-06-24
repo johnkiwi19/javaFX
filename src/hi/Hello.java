105ed8bd5808a0939f27adcdd4753a7ab10a7600
john <john@localhost>
2018-06-24 16:17:58 +1200

    Removed the unused imports

--- src/hi/Hello.java
+++ src/hi/Hello.java
@@ -6,12 +6,8 @@
 package hi;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 
 /**
  *
